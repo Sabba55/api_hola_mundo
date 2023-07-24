1ff9b0c62fa2b25a9449dd445153d1f33f1db049
Sabbatini Martin <martinurielsabbatini@gmail.com>
2023-07-24 20:39:19 -0300

    first commit

---
+++ app.js
@@ -0,0 +1,16 @@
+const { log } = require("console");
+const http = require("http");
+
+const hostname = "127.0.0.1";
+const port = 3000;
+
+const server = http.createServer((req, res) =>{
+    res.statusCode = 200;
+    res.setHeader = ("Content-Type", "application/json");
+    res.end('{ "mensaje": "HolaMundo!!"}');
+})
+
+server.listen(port, hostname, () => {
+    console.log(`Iniciando servidor en http://${hostname}:${port}/`);
+});
+// ejecutamos con node app.js
